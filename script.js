4451ff14d65b6a49df23aaca5c13c59e1823511b
Kirill <ki.karpachev@gmail.com>
2022-05-06 13:09:29 +0300

    Добавил функционал на кнопку

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const page = document.querySelector('.page');
 
+btn.addEventListener('click', function() {
+  let note = document.createElement('p');
+  note.textContent = input.value;
+  page.appendChild(note);
+})
