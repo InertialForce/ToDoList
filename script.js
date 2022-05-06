77f009d13962a203af9eae79d7519be308c8a199
Kirill <ki.karpachev@gmail.com>
2022-05-06 20:00:33 +0300

    Добавил шрифты, удаление заметок

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 const input = document.querySelector('.input');
 const btn = document.querySelector('.btn');
-const page = document.querySelector('.page');
+const containerNote = document.querySelector('.container-note');
 
-btn.addEventListener('click', function() {
-  let note = document.createElement('p');
-  note.textContent = input.value;
-  page.appendChild(note);
-})
+function createBox() {
+  if (input.value === '') {
+    alert('Введите заметку!');
+  } else {
+    const note = document.createElement('p');
+    note.className = 'note';
+    const btnClose = document.createElement('button');
+    btnClose.className = 'btn-close';
+    const noteBox = document.createElement('li');
+    noteBox.className = 'note-box'
+    note.textContent = input.value;
+    btnClose.textContent = 'x';
+    containerNote.appendChild(noteBox);
+    noteBox.appendChild(note);
+    noteBox.appendChild(btnClose);
+
+    btnClose.addEventListener('click', function () {
+      noteBox.parentNode.removeChild(noteBox);
+    })
+  }
+}
+
+btn.addEventListener('click', createBox);
